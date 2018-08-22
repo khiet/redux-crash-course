86431cb48918d67cb96512b370ac1fcf7ec03236
Khiet Le <khiet.le@ehochef.com>
2018-08-22 16:47:31 +0100

    setup Chrome Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,14 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
 
+// https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
